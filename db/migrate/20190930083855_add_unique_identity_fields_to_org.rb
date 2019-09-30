5bb130b74924a10983af6a5778008bd0f9ca268d
Armand <fardeauarmand@gmail.com>
2019-09-30 10:58:04 +0200

    Add migration and rake tasks

---
+++ db/migrate/20190930083855_add_unique_identity_fields_to_org.rb
@@ -0,0 +1,11 @@
+class AddUniqueIdentityFieldsToOrg < ActiveRecord::Migration[5.2]
+  def change
+    add_column :decidim_organizations, :unique_identity_methods, :string, array: true, default: ["online"]
+    add_column :decidim_organizations, :unique_identity_explanation_text, :jsonb, default: {}
+
+    # rubocop:disable Rails/SkipsModelValidations
+    Decidim::Organization.reset_column_information
+    Decidim::Organization.update_all(unique_identity_methods: ["online"])
+    # rubocop:enable Rails/SkipsModelValidations
+  end
+end
