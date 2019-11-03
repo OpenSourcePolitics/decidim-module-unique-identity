79401c87e247a0f2c5630b83e010ed3c2f8b5025
moustachu <git@moustachu.net>
2019-11-03 23:50:41 +0100

    update ressource url

--- app/events/decidim/unique_identity/authorization_acception_event.rb
+++ app/events/decidim/unique_identity/authorization_acception_event.rb
@@ -8,6 +8,12 @@ module Decidim
       def participatory_space_title
         @participatory_space_title ||= translated_attribute(resource.participatory_space.title)
       end
+
+      def resource_url
+        return super unless resource.is_a?(Decidim::Component)
+
+        @resource_url ||= [main_component_url(resource), resource.manifest, "new"].join("/")
+      end
     end
   end
 end
