2a0c1e6d1bedad626fbc6b7c4ebcf7d360659662
Armand <fardeauarmand@gmail.com>
2019-11-05 17:31:04 +0100

    Add email to officializations

--- spec/system/admin/offficializations/admin_manages_officializations_spec.rb
+++ spec/system/admin/offficializations/admin_manages_officializations_spec.rb
@@ -218,6 +218,22 @@ describe "Admin manages officializations", type: :system do
     end
   end
 
+  describe "displays email" do
+    let!(:user) { create(:user, organization: organization) }
+
+    before do
+      within ".secondary-nav" do
+        click_link "Participants"
+      end
+    end
+
+    it "redirect to conversation path" do
+      within "tr[data-user-id=\"#{user.id}\"]" do
+        expect(page).to have_content(user.email)
+      end
+    end
+  end
+
   describe "listing unique ids" do
     let!(:organization) do
       create(:organization, available_authorizations: ["unique_identity"])
